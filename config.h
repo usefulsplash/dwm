52476f412b7a9552360245b05b9fc5933f837b71
usefulsplash <usefulsplash@proton.me>
2025-02-10 19:29:57 -0500

    switch to ghostty

--- config.h
+++ config.h
@@ -83,7 +83,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "ghostty", NULL };
 static const char *browser[] = { "librewolf", NULL };
 static const char *notes[] = { "obsidian", NULL };
 static const char *watchmenu[] = { "watchmenu", NULL };
